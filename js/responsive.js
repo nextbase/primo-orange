d9d67c2f979dc7c169771d4c143d60b4d8a59e8b
nextbase <igor@next-aeon.com>
2015-09-17 16:56:17 +0900

    Deploying changes

--- js/responsive.js
+++ js/responsive.js
@@ -56,62 +56,66 @@ function initFilterExpand() {
 (function($) {
     $(document).ready(function() {
         var triggerBttn = $('#responsive-menu-trigger'),
-            overlay = $('#responsive-menu-overlay'),
-            closeBttn = overlay.find('button.overlay-close'),
-            navigationHeader = $('#responsive-header'),
-            transEndEventName = "webkitTransitionEnd transitionend oTransitionEnd MSTransitionEnd transitionend";
+                overlay = $('#responsive-menu-overlay'),
+                closeBttn = overlay.find( 'button.overlay-close'),
+                navigationHeader = $( '#responsive-header');
 
-        function toggleOverlay() {
-            if (overlay.hasClass('open')) {
-                overlay.removeClass('open');
-                navigationHeader.removeClass('open');
-                ensureMenusAreClosed();
-            } else {
-                overlay.addClass('open');
-                navigationHeader.addClass('open');
-            }
-        }
-
-        triggerBttn.click(function() {
-            toggleOverlay();
-        });
-
-        closeBttn.click(function() {
-            toggleOverlay();
-        });
-
-        // Menu Expands
-        function ensureMenusAreClosed() {
-            var firstLevelItems = $('#responsive-menu-overlay nav > ul > li');
-            firstLevelItems.removeClass('expanded');
-        }
-
-        function initJsExpands() {
-            var firstLevelItems = $('responsive-menu-overlay nav > ul > li'),
-                secondLevelItems = $('#responsive-menu-overlay nav > ul > li > ul > li');
-
-            firstLevelItems.each(function() {
-                if ($(this).children('ul').length != 0) {
-                    $(this).addClass('is-expandable');
-
+            function toggleOverlay() {
+                if( overlay.hasClass('open') ) {
+                    overlay.removeClass('open');
+                    navigationHeader.removeClass('open');
+                    ensureMenusAreClosed();
                 }
+                else if(navigationHeader.hasClass('open')) {
+                    overlay.removeClass('open');
+                    navigationHeader.removeClass('open');
+                    ensureMenusAreClosed();
+                }
+                else {
+                    overlay.addClass( 'open' );
+                    navigationHeader.addClass( 'open' );
+                }
+            }
+
+            triggerBttn.click( function() {
+                toggleOverlay();
             });
 
-            // Remove any accidental third level
-            secondLevelItems.each(function() {
-                $(this).find('ul').remove();
+            closeBttn.click( function() {
+                toggleOverlay();
             });
 
-            // Bind the links to expand / collapse
-            firstLevelItems.children('a').click(function(e) {
-                e.preventDefault();
-                $(this).parent().toggleClass('expanded');
-            })
-        }
+            // Menu Expands
+            function ensureMenusAreClosed() {
+                var firstLevelItems = $('#responsive-menu-overlay nav > ul > li, #responsive-menu-overlay nav > div > ul > li');
+                firstLevelItems.removeClass('expanded');
+            }
 
-        // JS Menu Expands Run
-        initJsExpands();
-        ensureMenusAreClosed();
+            function initJsExpands() {
+                var firstLevelItems = $('#responsive-menu-overlay nav > ul > li, #responsive-menu-overlay nav > div > ul > li'),
+                    secondLevelItems = $('#responsive-menu-overlay nav > ul > li > ul > li');
+
+                firstLevelItems.each(function() {
+                    if ($(this).children('ul').length != 0) {
+                        $(this).addClass('is-expandable');
+                    }
+                });
+
+                // Remove any accidental third level
+                secondLevelItems.each(function() {
+                    $(this).find('ul').remove();
+                });
+
+                // Bind the links to expand / collapse
+                firstLevelItems.children('a').click(function(e) {
+                    e.preventDefault();
+                    $(this).parent().toggleClass('expanded');
+                })
+            }
+
+            // JS Menu Expands Run
+            initJsExpands();
+            ensureMenusAreClosed();
     });
 
 })(jQuery);
