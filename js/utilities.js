efe3df501d078329ab0c5ad385aa2f2db4a50bda
nextbase <igor@next-aeon.com>
2015-09-20 22:57:27 +0900

    Updated utilities.js to allow the retur of the old styles

--- js/utilities.js
+++ js/utilities.js
@@ -9,6 +9,18 @@ function smoothReplaceCSS() {
     }, 800);
 }
 
+function oldStyles() {
+    oldCSS = $('link[href*="http://nextbase.github.io/primo-orange/css/main.css"]');
+    newCSS = oldCSS.clone();
+    newCSS.attr("href", "../wro/primo_library_wro_82UNIST_en_US.css");
+    newCSS.insertAfter(oldCSS);
+    setTimeout(function() {
+        console.log("JS: Replaced back the primo default!");
+        oldCSS.remove();
+    }, 800);
+}
+
+
 $(document).ready(function() {
     // Let's replace the CSS with the one we want
     smoothReplaceCSS();
