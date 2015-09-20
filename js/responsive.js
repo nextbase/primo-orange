960f7ddce382390314821e311f1a23829a706231
nextbase <igor@next-aeon.com>
2015-09-21 03:22:13 +0900

    Deploying changes

--- js/responsive.js
+++ js/responsive.js
@@ -110,6 +110,9 @@ $(document).ready(function() {
         toggleOverlay();
     });
 
+    // Account Buttons
+    accountLinks();
+
     // Menu Expands
     function ensureMenusAreClosed() {
         var firstLevelItems = $('#responsive-menu-overlay nav > ul > li, #responsive-menu-overlay nav > div > ul > li');
